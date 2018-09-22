5e6285697465267dc74550f535826453773714e0
zansea1 <43496583+zansea1@users.noreply.github.com>
2018-09-22 22:03:02 +0800

    Add files via upload

---
+++ mip.js
@@ -0,0 +1,22 @@
+var bj = document.getElementById('bj');
+var zk = document.getElementById('zk');
+$("#bj").click(function(event) {
+	if (bj.innerText == "更多") {
+		zk.style.height = "auto";
+		bj.innerText = "折叠";
+	} else {
+		zk.style.height = "30px";
+		bj.innerText = "更多";
+	}
+});
+var btn1 = document.getElementById('btn');
+var nav1 = document.getElementById('nav');
+$("#btn").click(function(event) {
+	if (btn1.innerText == "≡") {
+		nav1.style.width = '100%';
+		btn1.innerText = "×";
+	} else {
+		nav1.style.width = '0%';
+		btn1.innerText = "≡";
+	}
+});
